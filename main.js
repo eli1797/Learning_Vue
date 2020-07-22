bc3e274af5d60bad6699c81e80042fb40e280aa9
Eli <elibailey97@gmail.com>
2020-07-21 23:09:57 -0400

    render lists from objects

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ var app = new Vue({
 		image: 'assets/vmSocks-green-onWhite.jpg',
 		inventory: 9,
 		details: ["80% Cotton", "20% polyester", "Gender-neutral"],
+		variants: [
+			{
+				variantId: 2234,
+				variantColor: "green"
+			},
+			{
+				variantId: 2235,
+				variantColor: "blue"
+			}
+		],
 	}
 });
 
