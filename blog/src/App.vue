e80a227f2d14fe8082c5b9458d2d0d2b5e079354
Eli <elibailey97@gmail.com>
2020-07-31 19:55:45 -0400

    create header bar, functioning for both views

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <HeaderBar></HeaderBar>
     <router-view/>
   </div>
 </template>
 
+
+<script>
+  import HeaderBar from '@/components/HeaderBar.vue'
+
+  export default {
+    name: 'App',
+    components: { 
+      HeaderBar, 
+    },
+  }
+  
+</script>
 <style>
-/*#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}*/
 </style>
---
+++ blog/src/components/HeaderBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="breadcrumb" aria-label="breadcrumbs">
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+      <li class="is-active"><a href="#" aria-current="page">How to build a beautiful blog</a></li>
+    </ul>
+    <!-- <router-view/> -->
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'HeaderBar',
+  props: {}
+}
+</script>
