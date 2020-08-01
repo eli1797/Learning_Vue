cc977875ac32951769e3a981ef195adaa063c3cc
Eli <elibailey97@gmail.com>
2020-08-01 17:14:43 -0400

    writing editing about page

--- blog/src/views/About.vue
+++ blog/src/views/About.vue
@@ -5,14 +5,14 @@
         <div class="container">
           <h1 class="title">About</h1>
           <h2 class="subtitle">
-            I'm a dude
+            Stumbling into volleyball
           </h2>
 
           <div class="content">
             <p>
               First year of High School I failed to make the baseball team. I agreed to try volleyball after being asked. The head volleyball coach had a policy that upperclassmen had to bring a freshman to tryouts. I was tall so I made it despite being clumsy. I loved it. I'm still loving it many years later.
               <br><br>
-              Now I play and coach. Tall, skinny, and clumbsy me wasn't a very good player. Volleyball did not come naturally. Now, I'm grateful for those struggles. They sparked my interest in improving player developement, helped form my understanding of the game, and led me to coach. I write here to share my perspective and encourage discussion on these topics.
+              Now I play and coach. Tall, skinny, and clumbsy me wasn't a very good player. Volleyball did not come naturally. Now, I'm grateful for those struggles. They sparked my interest in improving player developement, helped form my understanding of the game, and led me to coach. I write here to help myself think, share my perspective, and encourage discussion about these topics.
             </p>
           </div>
         </div>
