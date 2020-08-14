6bb025199011b10f8e8ca314c93a1f14881527d6
Eli <elibailey97@gmail.com>
2020-08-14 00:09:56 -0400

    move back to left justify, content updates

--- blog/src/views/Blog.vue
+++ blog/src/views/Blog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="blog">
+  <div class="blog" style="max-width: 675px">
 
     <div class="container">
       <h1 class="title">Blog</h1>
@@ -11,36 +11,77 @@
     <div id="posts">
 
       <article id="fundamentals" class="section">
-        <div class="container" style="max-width: 650px">
-          <h1 class="title">Hitting Fundamentals</h1>
+        <div class="container">
+          <h1 class="title">Fundamental Skills</h1>
           <h2 class="subtitle">Awareness and Technique</h2>
 
           <div class="content">
-            <p>Great hitters are looking for kills regardless of the set, score, or opponent. Each set should be viewed as an opportunity/challenge to score.</p>
+            <p>You can have a lot of fun listening to the crowd at a high-level match. Amazed younger players exclaiming "I would die if that serve came at me" or "She jumps so high!” But my favorite phrase to hear is "How did they know that was going to happen!"</p>
             
-            <p>I’ll break down hitting into two skills, awareness and technique.</p>
+            <p>Dedicated athletes are impressive. Executing advanced technique with strength, grace, and consistency is no easy feat. But the real differentiator between professional and beginner is mental not physical. Domain experts focus their attention on more important factors, recognize patterns faster, and make better predictions about their teammates and opponents. This is awareness.</p>
             
-            <p>Technique is the ability to execute specific skills in a consistent manner.</p>
+            <p>I split nearly everything in sport into awareness and technique.</p>
             
-            <p>Awareness is the ability to read the game, recognize possibilities, and choose an appropriate technique to utilize.</p>
+            <p>Technique is the ability to execute specific skills (motor patterns) in a consistent manner.</p>
             
             <p>We practice technique. Hitting cross. Hitting line. Using the block to reset or tool. Tipping. These are the tools available to us. The motor patterns we practice until habitual.</p>
             
-            <p>Awareness is learned. It is seeing the game and understanding how to react.</p>
+            <p>Awareness is the ability to read the game, recognize possibilities, and choose a response.</p>
             
-            <p>We practice awareness by using peripheral vision to see where the block is and choose to hit line or cross around it. Awareness is noticing an opposing middle blocker playing back row defense and choosing to hit and tip at them. Awareness is recognizing a trap (tight and low) set against a big block and choosing to tip, wipe, or reset rather than swing.</p>
+            <p>We develop awareness too. It’s the skill of knowing what to look at, interpreting a most likely outcome, and choosing a reaction. It largely happens while you’re not touching the ball.</p>
             
-            <p>Each opportunity to attack should involve awareness, even if it’s only a glance at the block to see where to swing. Each hit should be intentional. Your attacking choice informed by the technique you’ve practiced and awareness of the situation. Recognize (awareness) then respond (technique).</p>
+            <p>The flow of sport is read, response. Awareness, technique. See, react.</p>
             
-            <p>If you hit with intent, getting blocked or dug is feedback. Perhaps you made the wrong choice or failed to see/recognize something. Video and coaches are also a great source of feedback.</p>
+            <p>To see this flow let’s look to some Olympians. Watch this clip:</p>
           
-            <p>To be a great hitter you should expand your toolbox but you also must recognize the job and know the right tool. Be intentional about your hitting and form habits around successful recognition-response patterns.</p>
+            <iframe width="100%" height="400px" src="https://www.youtube-nocookie.com/embed/bLCXHfyocVA?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br>
+
+            <p>We’ll focus on the action by Eric Shoji (near-side libero #22) and Micah Christenson. Wow. Watch the play again and only look at Shoji.</p>
+
+            <p>Now let's break down what Shoji does to set himself up to make this play.</p>
+
+            <p>He's playing middle back defense, so his first task figuring out where to position himself. Imagine a line splitting the court, hotdog style. Which side should he be on? How does he choose?</p>
+
+            <img style="width: 100%; min-width: 350px" src="../assets/shoji-middle-back.gif">
+
+            <p>I bet he watched the sharp angle of the hitters approach, see's his teammate's double block, and notices the inside set. (Awareness)</p>
+
+            <p>Then he does a tennis step right before the hitter contacts. This is a one-two step aimed to ready a player for movement. (Technique)</p>
+
+            <p>The rest is habit. Who should get the first ball, setter or libero? Shoji and Christenson both know.</p>
+
+            <img style="width: 100%; min-width: 350px" src="../assets/shoji-backup.gif">
+
+            <p>But there's a mistake and Shoji overpasses. Now, Christenson breaks the rules.</p>
+
+            <p>It will take time for Shoji to return to middle back and Brazil runs a quick offense. So Christenson takes middle back and (since he's deviated from the norm) communicates to Shoji.</p>
+
+            <img style="width: 100%; min-width: 350px" src="../assets/micah-shift.gif">
+
+            <p>So, what is awareness?</p>
+
+            <ul>
+              <li>Reading your opponents</li>
+              <ul>
+                <li>Example: Watching a hitter to position yourself on defense</li>
+              </ul>
+              <li>Acting with knowledge of your team's system</li>
+              <ul>
+                <li>Example: Libero takes first ball over setter</li>
+              </ul>
+              <li>Recognizing abnormal situations</li>
+              <ul>
+                <li>Example: Christenson shuffles over to give Shoji time to get up</li>
+              </ul>
+            </ul>
+
+            <p>It's great to watch pro-athletes. Unfortunately viewers tend to focus on who has the ball. Young players and scouting coaches could benefit from not "keeping their eye's on the ball." In my opinion, most of what makes an athlete great is in the anticipation, preparation, and focus show demonstrated by players out of the spotlight.</p>
           </div>
         </div>
       </article>
 
       <article id="awareness" class="section">
-        <div class="container" style="max-width: 650px">
+        <div class="container">
           <h1 class="title">How good are you at volleyball?</h1>
           <h2 class="subtitle">On evaluation</h2>
 
