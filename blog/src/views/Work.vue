d56a5a055bb09483c8a05ea65a4cf72329226aca
Eli <elibailey97@gmail.com>
2020-10-25 01:30:12 -0400

    content for sensory addition added, still needs links

--- blog/src/views/Work.vue
+++ blog/src/views/Work.vue
@@ -3,18 +3,20 @@
     <div class="container">
       <div class="box">
         <article class="media">
-          <div class="media-left">
-            <figure class="image is-128x128">
-              <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+          <div class="media-left is-hidden-mobile">
+            <figure class="image" style="max-width: 300px">
+              <img src="../assets/armband.jpg" alt="Image">
             </figure>
+
           </div>
           <div class="media-content">
             <div class="content">
-              <p>
-                <strong>John Smith</strong>
-                <br>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-              </p>
+              <p class="subtitle"><strong>Sensory Addition for Language</strong></p>
+              <figure class="image is-4by3 is-hidden-tablet">
+                <img src="../assets/armband.jpg" alt="Image">
+              </figure>
+              <p>Can we teach a tactile language? Can humans recognize syllables/words in vibration patterns on the forearm?</p>
+              <p>Part of a team that developed a haptic armband and ran a study teaching participants a language composed of audio syllables mapped to tactile sensations.</p>
             </div>
           </div>
         </article>
@@ -22,3 +24,10 @@
     </div>  
   </div>
 </template>
+
+<script>
+
+export default {
+  name: 'Work',
+}
+</script>
