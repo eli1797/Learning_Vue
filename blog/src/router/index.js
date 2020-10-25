25ac3c89c5d8ba6c729e3418debae2fc8990aba6
Eli <elibailey97@gmail.com>
2020-10-25 02:39:33 -0400

    add loci, fix typo in work view

--- blog/src/router/index.js
+++ blog/src/router/index.js
@@ -52,7 +52,7 @@ Vue.use(VueRouter)
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "blog" */ '../views/Work.vue'),
+    component: () => import(/* webpackChunkName: "work" */ '../views/Work.vue'),
     meta: {
       breadcrumb: "Work"
     }
--- blog/src/views/Work.vue
+++ blog/src/views/Work.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="work">
     <div class="container">
-      <div class="box">
+      <!-- Put work items here -->
+      <div id="sensory" class="box">
         <article class="media">
           <div class="media-left is-hidden-mobile">
             <figure class="image" style="max-width: 300px">
@@ -19,11 +20,37 @@
               <p>Our team developed a haptic armband and ran a study teaching participants a language composed of audio syllables mapped to tactile sensations.</p>
               <a href="https://docs.google.com/document/d/1J-lYN1S7y16q-tjPnKsI10jdSreZeVuEMSevx6IxUiI/edit?usp=sharing">Paper</a>
               <br>
-              <a href="https://www.youtube.com/watch?v=xmOC_orH56g">Project Video (terrible audio)</a>
+              <a href="https://www.youtube.com/watch?v=xmOC_orH56g">Project Video (really fantastic audio)</a>
             </div>
           </div>
         </article>
       </div>
+
+      <div id="loci" class="box">
+        <article class="media">
+          <div class="media-left is-hidden-mobile">
+            <figure class="image" style="max-width: 300px">
+              <img src="../assets/undraw_neighbors_ciwb.svg" alt="Image">
+            </figure>
+
+          </div>
+          <div class="media-content">
+            <div class="content">
+              <p class="subtitle"><strong>Loci</strong></p>
+              <figure class="image is-4by3 is-hidden-tablet">
+                <img src="../assets/undraw_neighbors_ciwb.svg" alt="Image">
+              </figure>
+              <p>An exploration of augmented signaling: What's next after body language, clothing, and accessories?</p>
+              <p>Goal: Help you find your people and have better interactions</p>
+              <p>What am I (a private person — though I do have a website) willing to share if it helps me and I completely own and control access to data like location and visibility?</p>
+              <a href="https://github.com/eli1797/loci_mvp">Github repo</a>
+              <br>
+              <a href="https://drive.google.com/drive/folders/1rDp0evbyQrMQvSB_bu-pnq0LLSBwNwHy?usp=sharing">Roam Notes</a>
+            </div>
+          </div>
+        </article>
+      </div>
+
     </div>  
   </div>
 </template>
