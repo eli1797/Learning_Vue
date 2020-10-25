151deb5b5c7b995c18dc1108fe59a968f39d1428
Eli <elibailey97@gmail.com>
2020-10-25 00:34:10 -0400

    A default work box (un-linked)

--- blog/src/views/Work.vue
+++ blog/src/views/Work.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="work">
-    <section>
-      <div class="container">
-        <h1>Work item</h1>
+    <div class="container">
+      <div class="box">
+        <article class="media">
+          <div class="media-left">
+            <figure class="image is-128x128">
+              <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+            </figure>
+          </div>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>John Smith</strong>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+              </p>
+            </div>
+          </div>
+        </article>
       </div>
-    </section>
+    </div>  
   </div>
 </template>
