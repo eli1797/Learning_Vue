3c16de6315bfa310e76e7d3d4cd1b74b6b6fcb0f
Eli <elibailey97@gmail.com>
2020-10-25 01:43:02 -0400

    add links to work

--- blog/src/views/Work.vue
+++ blog/src/views/Work.vue
@@ -15,8 +15,11 @@
               <figure class="image is-4by3 is-hidden-tablet">
                 <img src="../assets/armband.jpg" alt="Image">
               </figure>
-              <p>Can we teach a tactile language? Can humans recognize syllables/words in vibration patterns on the forearm?</p>
-              <p>Part of a team that developed a haptic armband and ran a study teaching participants a language composed of audio syllables mapped to tactile sensations.</p>
+              <p>Does language have to be heard or seen? Can humans recognize syllables/words in vibration patterns on the forearm?</p>
+              <p>Our team developed a haptic armband and ran a study teaching participants a language composed of audio syllables mapped to tactile sensations.</p>
+              <a href="https://docs.google.com/document/d/1J-lYN1S7y16q-tjPnKsI10jdSreZeVuEMSevx6IxUiI/edit?usp=sharing">Paper</a>
+              <br>
+              <a href="https://www.youtube.com/watch?v=xmOC_orH56g">Project Video (terrible audio)</a>
             </div>
           </div>
         </article>
